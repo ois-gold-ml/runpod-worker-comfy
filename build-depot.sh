d02378a85e51f49e4ed02402f64c8d5916df4ab2
Your Name <sergei.afonchenko42@gmail.com>
2025-04-14 18:03:48 -0300

    add models to image

--- build-depot.sh
+++ build-depot.sh
@@ -1,2 +1,3 @@
 LATEST_COMMIT_SHA=$(git rev-parse HEAD)
-depot build --push . -t fajyz/ois-gold-runpod-worker-comfy:${LATEST_COMMIT_SHA} --platform linux/amd64 --target=base
+depot build --push . -t fajyz/ois-gold-runpod-worker-comfy:${LATEST_COMMIT_SHA} --platform linux/amd64 --target=final \
+  --build-arg HUGGINGFACE_ACCESS_TOKEN="${HUGGINGFACE_ACCESS_TOKEN}"
