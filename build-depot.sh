0d7dfb62ed1e078c900cc1f0af3738237077554c
Your Name <sergei.afonchenko42@gmail.com>
2025-05-12 16:09:43 -0300

    update build-depot.sh

--- build-depot.sh
+++ build-depot.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Check if tag is provided
+if [ -z "$1" ]; then
+    echo "Error: Tag not provided"
+    echo "Usage: $0 <tag>"
+    exit 1
+fi
+
+TAG=$1
 LATEST_COMMIT_SHA=$(git rev-parse HEAD)
-depot build --push . -t fajyz/ois-gold-runpod-worker-comfy:${LATEST_COMMIT_SHA} --platform linux/amd64 --target=final \
-  --build-arg HUGGINGFACE_ACCESS_TOKEN="${HUGGINGFACE_ACCESS_TOKEN}"
+
+depot build --push . -t fajyz/ois-gold-runpod-worker-comfy:${TAG} --platform linux/amd64 --target=final \
